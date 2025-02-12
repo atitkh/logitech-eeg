f7d1d3ddb26e32984ce071cd447101fb35624371
Atit Kharel <akharel@ualr.edu>
2025-02-12 13:06:18 -0600

    Add initial implementation for LogitechController interaction

---
+++ main.py
@@ -0,0 +1,7 @@
+from logidrivepy import LogitechController
+
+controller = LogitechController()
+
+print(f"steering_initialize: {controller.steering_initialize()}")
+print(f"logi_update: {controller.logi_update()}")
+print(f"is_connected: {controller.is_connected(0)}")
