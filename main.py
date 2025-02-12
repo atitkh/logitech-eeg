f0b554be8718a13510fdedea6d6c331783d4ad84
Atit Kharel <akharel@ualr.edu>
2025-02-12 13:18:35 -0600

    Add continuous state retrieval for LogitechController

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ controller = LogitechController()
 print(f"steering_initialize: {controller.steering_initialize()}")
 print(f"logi_update: {controller.logi_update()}")
 print(f"is_connected: {controller.is_connected(0)}")
+
+while True:
+    values = controller.LogiGetStateENGINES(0)
+    print(values)
